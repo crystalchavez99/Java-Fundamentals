10ba58bdb3feb2fb872b7201ac99dd50bd0ff376
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-06-25 22:14:43 -0700

    add student route

--- Module-2/studentservice/src/main/java/com/company/studentservice/controller/StudentServiceController.java
+++ Module-2/studentservice/src/main/java/com/company/studentservice/controller/StudentServiceController.java
@@ -11,23 +11,48 @@ import java.util.ArrayList;
 
 @RestController
 public class StudentServiceController {
-    List <Student> studentList = new ArrayList<>();
-
-    //This data would come from your db in real life
+    // List of students to mock a db
+    List<Student> studentList = new ArrayList<>();
+    // This data would come from your db in real life
+    // populate the studentlist with Students via the StudentSercieController Constructor
     public StudentServiceController(){
         studentList.add(new Student("Sam"));
         studentList.add(new Student("Lisa"));
         studentList.add(new Student("Ming"));
         studentList.add(new Student("Lynda"));
     }
-
+    // route to get all students
+    // http method: get
+    // uri: /students
     @RequestMapping(value = "/students", method = RequestMethod.GET)
-    public List<Student> getStudents() {
+    public List<Student> getStudents(){
         return studentList;
     }
-
+    // route to get a student by index
+    // http method: get
+    // uri: /students/{index}
     @RequestMapping(value = "/students/{index}", method = RequestMethod.GET)
-    public Student getStudentByIndex(@PathVariable int index) {
+    public Student getStudentByIndex(@PathVariable int index){
         return studentList.get(index);
     }
+    // route to get a student by name
+    // http method: get
+    // uri: /studentsByName/{name}
+    @RequestMapping(value="/studentsByName/{name}", method = RequestMethod.GET)
+    public Student getStudentByName(@PathVariable String name){
+        Student selectedStudent = null;
+        for (Student student: studentList){
+            if(student.getName().equals(name)){
+                selectedStudent = student;
+            }
+        }
+        return selectedStudent;
+    }
+
+
+
+
+
+
+
 }
