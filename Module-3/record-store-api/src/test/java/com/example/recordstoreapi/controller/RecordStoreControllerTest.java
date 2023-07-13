2db5840532e03eae76e712d2cacd04059a4ab728
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-07-13 10:09:13 -0700

    mockmvc test strategy

--- Module-3/record-store-api/src/test/java/com/example/recordstoreapi/controller/RecordStoreControllerTest.java
+++ Module-3/record-store-api/src/test/java/com/example/recordstoreapi/controller/RecordStoreControllerTest.java
@@ -3,13 +3,19 @@ import com.example.recordstoreapi.models.Record;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RecordStoreController.class)
 public class RecordStoreControllerTest {// Wiring in the MockMvc object
@@ -26,4 +32,49 @@ public class RecordStoreControllerTest {// Wiring in the MockMvc object
     public void setUp() {
         // Standard set up method for instantiating test objects
         // Don't have to do anything special for mockMvc since it's Autowired
-    }}
+    }
+    // Testing GET /records
+    @Test
+    public void shouldReturnAllRecordsInCollection() throws Exception {
+
+        // ARRANGE
+        // Convert Java object to JSON
+        String outputJson = mapper.writeValueAsString(recordList);
+
+        // ACT
+        mockMvc.perform(get("/records"))        // Perform the GET request
+                .andDo(print())              // Print results to console
+                .andExpect(status().isOk());        // ASSERT (status code is 200)
+    }
+
+    // Testing POST /records
+    @Test
+    public void shouldReturnNewRecordOnPostRequest() throws Exception {
+
+        // ARRANGE
+        Record inputRecord = new Record();
+        inputRecord.setArtist("Bruce Springsteen");
+        inputRecord.setAlbum("The River");
+        inputRecord.setYear("1990");
+
+        // Convert Java Object to JSON
+        String inputJson = mapper.writeValueAsString(inputRecord);
+
+        Record outputRecord = new Record();
+        outputRecord.setArtist("Bruce Springsteen");
+        outputRecord.setAlbum("The River");
+        outputRecord.setYear("1990");
+        outputRecord.setId(6);
+
+        String outputJson = mapper.writeValueAsString(outputRecord);
+
+        // ACT
+        mockMvc.perform(
+                        post("/records")              // Perform the POST request
+                                .content(inputJson)             // Set the request body
+                                .contentType(MediaType.APPLICATION_JSON)  // Tell the server it's in JSON format
+                )
+                .andDo(print())                // Print results to console
+                .andExpect(status().isCreated());        // ASSERT (status code is 201)
+    }
+}
