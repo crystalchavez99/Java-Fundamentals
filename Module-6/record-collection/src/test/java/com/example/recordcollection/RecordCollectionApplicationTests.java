929eb1454313fd19581baae511d848009b3493ce
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-07-25 09:52:19 -0700

    module 6 simple crm

---
+++ Module-6/record-collection/src/test/java/com/example/recordcollection/RecordCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recordcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
