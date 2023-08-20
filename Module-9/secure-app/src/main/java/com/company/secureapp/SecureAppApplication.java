f4ad55084d4cf4d06e4ea2f81d33c81a7ecd7d37
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-08-20 12:32:38 -0700

    set up secure app

--- Module-9/secure-app/src/main/java/com/company/secureapp/SecureAppApplication.java
+++ Module-9/secure-app/src/main/java/com/company/secureapp/SecureAppApplication.java
@@ -2,8 +2,10 @@ package com.company.secureapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// turn off default security
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class SecureAppApplication {
 
 	public static void main(String[] args) {
