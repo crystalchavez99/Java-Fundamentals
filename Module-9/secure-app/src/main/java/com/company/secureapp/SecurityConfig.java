03cd17296cf1c3ac7f544f0a5cd517aa7c55b75b
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-08-20 21:02:21 -0700

    applied spring security

---
+++ Module-9/secure-app/src/main/java/com/company/secureapp/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.company.secureapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+// annotation indicates that this is a configuration class.
+@Configuration
+//autowiring the data source configured in the previous step
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
+
+        // This will cause Spring Security to apply the BCrypt hashing algorithm to incoming
+        // passwords before comparing them to the value in the database.
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        // AuthenticationManagerBuilder to configure Spring Security to use the database schema created previously
+        authBuilder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username = ?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username = ?")
+                .passwordEncoder(encoder);
+
+    }
+}
